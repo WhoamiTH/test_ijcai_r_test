f3a5baed6e0fc79d79512b55270efbff2cc585fc
taihan <taihan@bytedance.com>
2022-08-07 01:14:10 +0800

    first commit

---
+++ del_file.sh
@@ -0,0 +1,196 @@
+#!/bin/bash
+set -e
+
+
+rm -rf ./test_glass5/model_MLP_normal_15000/
+rm -rf ./test_glass5/result_MLP_normal_15000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_normal_15000/
+rm -rf ./test_glass5/result_MLP_normal_15000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_normal_15000/
+rm -rf ./test_glass5/result_MLP_normal_15000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_normal_15000/
+rm -rf ./test_glass5/result_MLP_normal_15000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_normal_8000/
+rm -rf ./test_glass5/result_MLP_normal_8000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_normal_8000/
+rm -rf ./test_glass5/result_MLP_normal_8000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_normal_8000/
+rm -rf ./test_glass5/result_MLP_normal_8000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_normal_8000/
+rm -rf ./test_glass5/result_MLP_normal_8000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_bm_15000/
+rm -rf ./test_glass5/result_MLP_bm_15000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_bm_15000/
+rm -rf ./test_glass5/result_MLP_bm_15000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_bm_15000/
+rm -rf ./test_glass5/result_MLP_bm_15000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_bm_15000/
+rm -rf ./test_glass5/result_MLP_bm_15000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_bm_8000/
+rm -rf ./test_glass5/result_MLP_bm_8000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_bm_8000/
+rm -rf ./test_glass5/result_MLP_bm_8000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_bm_8000/
+rm -rf ./test_glass5/result_MLP_bm_8000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_bm_8000/
+rm -rf ./test_glass5/result_MLP_bm_8000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im_15000/
+rm -rf ./test_glass5/result_MLP_im_15000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im_15000/
+rm -rf ./test_glass5/result_MLP_im_15000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im_15000/
+rm -rf ./test_glass5/result_MLP_im_15000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im_15000/
+rm -rf ./test_glass5/result_MLP_im_15000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im_8000/
+rm -rf ./test_glass5/result_MLP_im_8000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im_8000/
+rm -rf ./test_glass5/result_MLP_im_8000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im_8000/
+rm -rf ./test_glass5/result_MLP_im_8000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im_8000/
+rm -rf ./test_glass5/result_MLP_im_8000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im2_15000/
+rm -rf ./test_glass5/result_MLP_im2_15000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im2_15000/
+rm -rf ./test_glass5/result_MLP_im2_15000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im2_15000/
+rm -rf ./test_glass5/result_MLP_im2_15000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im2_15000/
+rm -rf ./test_glass5/result_MLP_im2_15000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im2_8000/
+rm -rf ./test_glass5/result_MLP_im2_8000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im2_8000/
+rm -rf ./test_glass5/result_MLP_im2_8000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im2_8000/
+rm -rf ./test_glass5/result_MLP_im2_8000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im2_8000/
+rm -rf ./test_glass5/result_MLP_im2_8000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im3_15000/
+rm -rf ./test_glass5/result_MLP_im3_15000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im3_15000/
+rm -rf ./test_glass5/result_MLP_im3_15000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im3_15000/
+rm -rf ./test_glass5/result_MLP_im3_15000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im3_15000/
+rm -rf ./test_glass5/result_MLP_im3_15000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im3_8000/
+rm -rf ./test_glass5/result_MLP_im3_8000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im3_8000/
+rm -rf ./test_glass5/result_MLP_im3_8000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im3_8000/
+rm -rf ./test_glass5/result_MLP_im3_8000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_im3_8000/
+rm -rf ./test_glass5/result_MLP_im3_8000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both_15000/
+rm -rf ./test_glass5/result_MLP_both_15000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both_15000/
+rm -rf ./test_glass5/result_MLP_both_15000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both_15000/
+rm -rf ./test_glass5/result_MLP_both_15000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both_15000/
+rm -rf ./test_glass5/result_MLP_both_15000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both_8000/
+rm -rf ./test_glass5/result_MLP_both_8000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both_8000/
+rm -rf ./test_glass5/result_MLP_both_8000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both_8000/
+rm -rf ./test_glass5/result_MLP_both_8000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both_8000/
+rm -rf ./test_glass5/result_MLP_both_8000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both2_15000/
+rm -rf ./test_glass5/result_MLP_both2_15000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both2_15000/
+rm -rf ./test_glass5/result_MLP_both2_15000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both2_15000/
+rm -rf ./test_glass5/result_MLP_both2_15000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both2_15000/
+rm -rf ./test_glass5/result_MLP_both2_15000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both2_8000/
+rm -rf ./test_glass5/result_MLP_both2_8000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both2_8000/
+rm -rf ./test_glass5/result_MLP_both2_8000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both2_8000/
+rm -rf ./test_glass5/result_MLP_both2_8000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both2_8000/
+rm -rf ./test_glass5/result_MLP_both2_8000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both3_15000/
+rm -rf ./test_glass5/result_MLP_both3_15000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both3_15000/
+rm -rf ./test_glass5/result_MLP_both3_15000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both3_15000/
+rm -rf ./test_glass5/result_MLP_both3_15000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both3_15000/
+rm -rf ./test_glass5/result_MLP_both3_15000_both_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both3_8000/
+rm -rf ./test_glass5/result_MLP_both3_8000_normal_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both3_8000/
+rm -rf ./test_glass5/result_MLP_both3_8000_bm_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both3_8000/
+rm -rf ./test_glass5/result_MLP_both3_8000_im_num_10_half
+
+rm -rf ./test_glass5/model_MLP_both3_8000/
+rm -rf ./test_glass5/result_MLP_both3_8000_both_num_10_half
+
