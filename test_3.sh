c9ef6a0a7d05e66c1095842e376ed5eef874d98a
taihan <taihan@bytedance.com>
2022-08-08 16:14:11 +0800

    change device

--- test_3.sh
+++ test_3.sh
@@ -6,61 +6,61 @@ set -e
 
 
 mkdir -p ./test_yeast5/result_MLP_both_8000_both_num_10_half/record_1/
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both_8000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both_8000 test_method=both_num_10_half device_id=0
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both_8000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both_8000 test_method=both_num_10_half device_id=1
 
 
 
 mkdir -p ./test_yeast5/result_MLP_both_10000_both_num_10_half/record_1/
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=0
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both_10000 test_method=both_num_10_half device_id=1
 
 
 
 mkdir -p ./test_yeast5/result_MLP_both_15000_both_num_10_half/record_1/
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=0
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both_15000 test_method=both_num_10_half device_id=1
 
 
 
 mkdir -p ./test_yeast5/result_MLP_both_20000_both_num_10_half/record_1/
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=0
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both_20000 test_method=both_num_10_half device_id=1
 
 
 
 mkdir -p ./test_yeast5/result_MLP_both2_5000_normal_num_10_half/record_1/
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both2_5000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both2_5000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both2_5000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both2_5000 test_method=normal_num_10_half device_id=0
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both2_5000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both2_5000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both2_5000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both2_5000 test_method=normal_num_10_half device_id=1
 
 
 
 mkdir -p ./test_yeast5/result_MLP_both2_8000_normal_num_10_half/record_1/
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=0
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both2_8000 test_method=normal_num_10_half device_id=1
 
 
 
 mkdir -p ./test_yeast5/result_MLP_both2_10000_normal_num_10_half/record_1/
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=0
-python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=0
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=1 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=2 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=3 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=4 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=1
+python3 ./classifier_MLP/test.py dataset_name=yeast5 dataset_index=5 record_index=1 train_method=MLP_both2_10000 test_method=normal_num_10_half device_id=1
 
 
 
